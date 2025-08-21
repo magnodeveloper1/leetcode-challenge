72b5af269a59f7e90fe7aae7cb299b012342faaf
Cristiano Mbongo <52001391+magnodeveloper1@users.noreply.github.com>
2025-08-21 16:51:03 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,3 +1,5 @@
+#https://www.hackerrank.com/challenges/merge-the-tools/problem?isFullScreen=true
+
 def merge_the_tools(s, k):
     # Percorre s em blocos de tamanho k
     for i in range(0, len(s), k):
