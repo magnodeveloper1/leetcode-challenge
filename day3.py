b65f2844f31e20312ceaa5bc1ae7760900e5b8c9
Cristiano Mbongo <cristiano.mbongoexit>
2025-08-22 08:48:24 +0100

    ds

--- day3.py
+++ day3.py
@@ -1,7 +1,6 @@
 #https://www.hackerrank.com/challenges/merge-the-tools/problem?isFullScreen=true
 
 def merge_the_tools(s, k):
-    # Percorre s em blocos de tamanho k
     for i in range(0, len(s), k):
         substring = s[i:i + k]
         seen = set()
